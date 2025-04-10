329042862974baa0384425f193764efabb5831e7
Zaffot <Bror.torstensson@student.hamk.fi>
2025-04-10 19:02:49 +0300

    add BasicSettings

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=> {
+    res.send("Testing");
+}); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App listning on port ${PORT}`));
