31a1fa20925f71533273f63213f5e0f40c6185dc
Zaffot <Bror.torstensson@student.hamk.fi>
2025-04-10 20:44:29 +0300

    add homepage

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 
+
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine','handlebars');
+
 app.get('/',(req,res)=> {
-    res.send("Testing");
+    //res.send("Testing");
+    res.render('index');
 }); 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listning on port ${PORT}`));
