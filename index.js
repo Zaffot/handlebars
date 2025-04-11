3c82078cac15b04cb70ba5a64a6dd75d93b5bdb7
Zaffot <Bror.torstensson@student.hamk.fi>
2025-04-11 20:46:22 +0300

    add newPagesAndRoutes

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const exphbs = require('express-handlebars');
 
 
 const app = express();
-
+// dummy database IRL we would get data form MongoDB
+const products = [
+    {
+        name: 'Kia',
+        price: 25000
+    },
+    {
+        name: 'Volvo',
+        price: 30000
+    }
+]
 //starts handlebars
 app.engine('handlebars', exphbs.engine({
     defaultLayout: 'main'
@@ -14,17 +24,35 @@ app.set('view engine','handlebars');
 //route to page
 app.get('/',(req,res)=> {
     //res.send("Testing");
-    res.render('index');
+    //res.render('index');
+    res.render('index',{
+        title: 'Home',
+        companyName: 'Business Ltd'
+    });
 }); 
+
 
 //route to contact page
 app.get('/contact',(req,res)=> {
     res.render('contact');
 }); 
 
+// route to products page 
+app.get('/products',(req,res)=>{
+    res.render('products',
+    {
+        title: "Products",
+        products: products
+    })
+});
+
+
 //folder to staric files like css jpg etc
 app.use(express.static('public'));
 
+app.use((req,res,next) => {
+    res.status(404).send("Sorry, could not find the content");
+});
 
 
 //port 
